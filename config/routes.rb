cc180dec21c6bc320de8d6aff890230d92c81e88
kmtszky <kmtszk711@gmail.com>
2021-03-27 10:27:37 +0000

    [update] devise用strong parameter設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/about'
+  root 'homes#top'
   devise_for :users
+  resources :homes, only: [:top, :about]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
