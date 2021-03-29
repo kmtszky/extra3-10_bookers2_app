577c42fcb5aa46216ac8994c75ad440db279d61c
kmtszky <kmtszk711@gmail.com>
2021-03-29 10:40:54 +0000

    [fix] books indexへのエラーメッセージ実装修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,7 +35,6 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
     else
-      @book = @error
       render :edit
     end
   end
