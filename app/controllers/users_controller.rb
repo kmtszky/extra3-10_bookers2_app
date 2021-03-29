86fca0ebb01a47a225a6093cc0617114e0b2a8d9
kmtszky <kmtszk711@gmail.com>
2021-03-29 08:22:01 +0000

    [update] userコントローラー定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
+  before_action :user_find, only: [:show, :edit, :update]
+
   def show
   end
 
   def index
+    @users = User.all
   end
 
   def edit
+    @user.save
+    redirect_to user_path(@user.id)
   end
 
   def update
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_find
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
