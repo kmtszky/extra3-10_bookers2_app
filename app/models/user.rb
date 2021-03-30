9ca36ff9c68f4eb708812baca67853ab7d1da754
kmtszky <kmtszk711@gmail.com>
2021-03-30 03:44:52 +0000

    [update] アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   attachment :profile_image
+
   has_many :books, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
