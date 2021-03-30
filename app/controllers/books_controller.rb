58bcea664d81d81ed6832dee206c1223579b7a08
kmtszky <kmtszk711@gmail.com>
2021-03-30 09:15:31 +0000

    [update] books/showページへコメントフォーム、コメント一覧追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @user = @book.user
+    @comment = Comment.new
   end
 
   def index
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   def create
-    
   end
   
   def destroy
