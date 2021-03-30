bfb7a3e6cc0294a0e3e347398a5ae3e26d81aa94
kmtszky <kmtszk711@gmail.com>
2021-03-30 07:38:47 +0000

    [update] routing：ネストされたものへ修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :index, :edit, :update]
 
-  # books
-  resources :books, only: [:show, :index, :create, :edit, :update, :destroy]
-
-  # comments
-  resources :comments, only: [:create, :destroy]
+  # books, comments
+  resources :books, only: [:show, :index, :create, :edit, :update, :destroy] do
+    resources :comments, only: [:create, :destroy]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
