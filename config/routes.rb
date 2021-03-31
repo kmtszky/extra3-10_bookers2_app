abe46f5657c9adb415a765fd8d2b03d45d4cb3e9
kmtszky <kmtszk711@gmail.com>
2021-03-31 08:39:23 +0000

    [Add] favoriteルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # books, comments
   resources :books, only: [:show, :index, :create, :edit, :update, :destroy] do
     resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
