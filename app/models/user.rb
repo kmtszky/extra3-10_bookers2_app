2a4ea6b2c6716e815dbbfc9274944f05739d256e
kmtszky <kmtszk711@gmail.com>
2021-04-02 15:41:30 +0000

    [fix] path修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   # @user.followerとした際に、@user.idをfollowed_idと指定 = フォローしている人を取得
-  has_many :follower, class_name: "Relationship", foregin_key: "follower_id", dependent: :destroy #follower_idにuser_idを格納する: フォローしている人を取得
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #follower_idにuser_idを格納する: フォローしている人を取得
   # @user.followedとした際に、@user.idをfollowed_idと指定 = フォロワー取得
   has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy #followed_idにuser_idを格納する：フォロワーを取得
 
