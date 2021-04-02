16845fd9d9ae62bc2b54617ca3718cb1c671b100
kmtszky <kmtszk711@gmail.com>
2021-04-02 04:58:07 +0000

    [update] validation時エラーメッセージ追加（flash使用）

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,16 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to book_path(@book)
+    else
+      flash[:notice] = "Please enter a comment of at least one character"
+      @book = Book.find(params[:book_id])
+      @user = @book.user
+      render 'books/show'
+    end
   end
 
   def destroy
