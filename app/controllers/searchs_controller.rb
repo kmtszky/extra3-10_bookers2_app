bfb8ec70b763edb69dac5f881f9c4903aefe4a10
kmtszky <kmtszk711@gmail.com>
2021-04-05 04:58:35 +0000

    [Add] 課題5: user_model => searchメソッド定義

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -2,14 +2,18 @@ class SearchsController < ApplicationController
   def index
     #キーワードが入力されていれば、whereメソッドとLIKE検索（部分一致検索）を組み合わせて、必要な情報のみ取得する。
     if params[:search_key]
-      @results = Book.where('title LIKE ?', "%#{params[:search_key]}%").where('body LIKE ?', "%#{params[:search_key]}%")
+      @category = params[:category]
+      search = params[:search]
+      search_key = params[:search_key]
+
+      if @category == 'user_model'
+        @user = User.search(search, search_key)
+      else
+        @book = Book.search(search, searcsh_key)
+      end
     else
-      @results = Book.all
+      flash[:ellors_explanation] = 'please input some words'
+      redirect_back(fallback_location: books_path)
     end
   end
-  
-  private
-  
-  def select(book, user)
-    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,17 @@ class User < ApplicationRecord
     self.following_users.include?(other_user)
   end
 
+  def self.search(search, search_key)
+    if search == perfect_match
+      @user = User.where(name: "#{search_key}")
+    elsif search == forward_matsh
+      @user = User.where("name like?", "%#{search_key}")
+    elsif search == backward_match
+      @user = User.where("name like?", "#{search_key}%")
+    elsif search == partial_match
+      @user = User.where("name like?", "%#{sesarch_key}%")
+    else
+      @user = User.all
+    end
+  end
 end
