b47db60686110af679fef451028b14e27d5580af
kmtszky <kmtszk711@gmail.com>
2021-04-08 07:30:39 +0000

    [fix] 課題7: 動かなくなっているので見直し #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resources)
+  def after_sign_in_path_for(resource)
     user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
 
   protected
@@ -13,14 +17,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [
-      :email,
-      :name,
-      :postcode,
-      :prefecture_name,
-      :address_city,
-      :address_street,
-      :address_building
-    ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :postal_code, :prefecture_name, :address_city, :address_street, :address_building])
   end
 end
