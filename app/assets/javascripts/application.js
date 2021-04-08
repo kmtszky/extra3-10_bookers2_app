61e172b689a7e5e1b1d0da13512cd6f01d1a35e9
kmtszky <kmtszk711@gmail.com>
2021-04-08 07:15:22 +0000

    [fix] 課題7: 動かなくなっているので見直し #2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
-
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
@@ -21,14 +20,14 @@
 
 $(function () {
   $(document).on('turbolinks:load', () => {
-    $('#user_postal_code').jpostal({
+    $('#user_postcode').jpostal({
       postcode: [
-        '#user_postal_code'
+        '#user_postcode'
       ],
       address: {
         "#user_prefecture_code": "%3", // # 都道府県が入力される
-        "#user_city"           : "%4%5", // # 市区町村と町域が入力される
-        "#user_street"         : "%6%7" // # 大口事務所の番地と名称が入力される
+        "#user_address_city": "%4%5", // # 市区町村と町域が入力される
+        "#user_address_street": "%6%7" // # 大口事務所の番地と名称が入力される
       }
     });
   });
