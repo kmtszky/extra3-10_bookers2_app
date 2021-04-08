79a94f380040c8a8680e3058bad47c0ca6090779
kmtszky <kmtszk711@gmail.com>
2021-04-08 01:47:45 +0000

    [update] モデル定義

--- app/mailers/thanx_mailer.rb
+++ app/mailers/thanx_mailer.rb
@@ -1,2 +1,8 @@
 class ThanxMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def thanx_email
+   @user = params[:user]
+   mail(to: @user.email, subject: 'welcome to bookers2! Thenkx for your registration.')
+  end
 end
