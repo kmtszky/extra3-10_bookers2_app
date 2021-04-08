85e5d75b231a9090fd3a15bc0bf36f4692142868
kmtszky <kmtszk711@gmail.com>
2021-04-08 06:22:34 +0000

    [Add] devise user controller

--- app/mailers/thanx_mailer.rb
+++ app/mailers/thanx_mailer.rb
@@ -1,7 +1,7 @@
 class ThanxMailer < ApplicationMailer
   default from: 'notifications@example.com'
 
-  def thanx_email
+  def thanx_mail
    @user = params[:user]
    mail(to: @user.email, subject: 'welcome to bookers2! Thenkx for your registration.')
   end
