a66a858e7afae3871d18c87eed70e27de659e0d1
kmtszky <kmtszk711@gmail.com>
2021-04-09 08:31:40 +0000

    [update] 課題9 chat showページへのリンク作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books
+    @cu_rooms = UserRoom.where(user_id: current_user) #current_userが参加しているルームをすべて取り出す
+    @u_rooms = UserRoom.where(user_id: @user.id)      #@userが参加しているルームをすべて取り出す
+    unless current_user.id == @user.id
+      @cu_rooms.each do |cu_room| #current_userが参加しているルームをひとつずつcu_roomへ格納
+        @u_rooms.each do |u_room| #@userが参加しているルームをひとつずつu_roomへ格納
+          if cu_room.room_id == u_room.room_id
+            @room_id = cu_room.room_id
+            @have_room = true
+          else
+            @room = Room.new
+            @user_room = UserRoom.new
+          end
+        end
+      end
+    end
   end
 
   def index
