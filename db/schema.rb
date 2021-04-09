ec1252e02febd5d761dee9ba1c4d033deab0bc9e
kmtszky <kmtszk711@gmail.com>
2021-04-09 05:11:37 +0000

    [Update] 課題9 アソシエーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_043800) do
+ActiveRecord::Schema.define(version: 2021_04_09_044004) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -51,6 +51,13 @@ ActiveRecord::Schema.define(version: 2021_04_09_043800) do
   end
 
   create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
