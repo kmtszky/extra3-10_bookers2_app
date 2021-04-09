a1045d690be02500720fa51ab95f17ff8bbdb31d
kmtszky <kmtszk711@gmail.com>
2021-04-09 04:32:00 +0000

    [Start]課題9 Room model作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_075535) do
+ActiveRecord::Schema.define(version: 2021_04_09_043126) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 2021_04_07_075535) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,11 +58,11 @@ ActiveRecord::Schema.define(version: 2021_04_07_075535) do
     t.datetime "updated_at", null: false
     t.text "introduction"
     t.string "profile_image_id"
-    t.integer "postcode"
     t.integer "prefecture_code"
     t.string "address_city"
     t.string "address_street"
     t.string "address_building"
+    t.integer "postcode"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
